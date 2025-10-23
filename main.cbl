90fadb25f6e502c50beef34c2881b74a86192fe7
JoshuaBOUCHAT <joshuabouchat@gmail.com>
2025-10-23 13:39:41 +0200

    some changes

--- main.cbl
+++ main.cbl
@@ -1,24 +1,24 @@
        identification division.
        program-id. my_program.
        data division.
-       local-storage section.
-
        working-storage section.
        
        01 WS-STUDENTS.
            02 WS-STUDENT OCCURS 6 TIMES.
-               03 WS-STUDENT-NAME PIC X(25) VALUE "John".
-               03 WS-STUDENT-SURNAME PIC X(25) VALUE "Doe".
-               03 WS-STUDENT-ID PIC 9(8) VALUE 0.
+               03 WS-STUDENT-NAME      PIC X(25)   VALUE "John".
+               03 WS-STUDENT-SURNAME   PIC X(25)   VALUE "Doe".
+               03 WS-STUDENT-ID        PIC 9(8)    VALUE 0.
+       local-storage section.
+	   
+	   01 I pic 9.    
+
+   
 
        procedure division.
-           display "Entre le nombre de puissance de 2 ajoute"
-           accept N
-           perform until I>N
-               add I-VAL to LS-SUM 
-               divide I-VAL by 2 giving I-VAL
-               display "La valeur de la somme apres ",N "iteration",
-                   "est de: ",LS-SUM
-               add 1 to I
-           end-perform.
+           perform varying I from 1 by 1 until I>6
+               display "Nom: ",WS-STUDENT-SURNAME(1),
+               "Prenom: ", WS-STUDENT-NAME(1),
+               "id: ",WS-STUDENT-ID(1)
+		   end-perform.
+           
        stop run.
