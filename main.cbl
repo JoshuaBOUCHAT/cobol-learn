7d42744f05f7c53ad1caa8d380f815fde35be477
JoshuaBOUCHAT <joshuabouchat@gmail.com>
2025-10-24 14:05:57 +0200

    start to impl langford

--- main.cbl
+++ main.cbl
@@ -3,22 +3,30 @@
        data division.
        working-storage section.
        
-       01 WS-STUDENTS.
-           02 WS-STUDENT OCCURS 6 TIMES.
-               03 WS-STUDENT-NAME      PIC X(25)   VALUE "John".
-               03 WS-STUDENT-SURNAME   PIC X(25)   VALUE "Doe".
-               03 WS-STUDENT-ID        PIC 9(8)    VALUE 0.
-       local-storage section.
+
+	   01 WS-STATES
+	      05 WS-STACK-SIZE COMP value 0.
+	      05 WS-STATE occurs 512 times.
+	          10 WS-STATE-RANK PIC 99.
+	          10 WS-PATTERN occurs 32 times.
+		         15 WS-PATTERN-VAL PIC 9 value 0.
 	   
-	   01 I pic 9.    
-
-   
-
+	   01 N PIC 99 value 0.
+	   01 N2 PIC 99 value 0.
        procedure division.
-           perform varying I from 1 by 1 until I>6
-               display "Nom: ",WS-STUDENT-SURNAME(1),
-               "Prenom: ", WS-STUDENT-NAME(1),
-               "id: ",WS-STUDENT-ID(1)
+
+	       perform until N>0 AND N <17
+	       display "Donne moi un nombre entre 1 et 16 compris"
+		   accept N
+		   if N < 1 OR N > 16
+		     display "Nombre trop petit !"
+		   end-if.
+		   add N to N giving N2
+		   move 1 to WS-STACK-SIZE
+
+
+		   perform until WS-STACK-SIZE equal 0
+               
 		   end-perform.
-           
        stop run.
+       
